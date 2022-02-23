36f79af7a9c3a27cb2c156fc6515ecee87d6ff0c
pinke <pinke.yu7@gmail.com>
2022-02-23 16:03:31 +0800

    Feature: contact api and routes

--- api/v1/contact_api.go
+++ api/v1/contact_api.go
@@ -8,7 +8,36 @@ import (
 	"address-book-go/pkg/valider"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
+
+func AddContact(c *gin.Context) {
+	req := apireq.AddContact{}
+	err := c.BindJSON(&req)
+	if err != nil {
+		err = er.NewAppErr(400, er.ErrorParamInvalid, err.Error(), err)
+		_ = c.Error(err)
+		return
+	}
+
+	// 參數驗證
+	err = valider.Validate.Struct(req)
+	if err != nil {
+		err = er.NewAppErr(400, er.ErrorParamInvalid, err.Error(), err)
+		_ = c.Error(err)
+		return
+	}
+
+	cr := contactRepo.NewRepository()
+	cs := contactSrv.NewService(cr)
+	err = cs.Add(&req)
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{})
+}
 
 func ListContact(c *gin.Context) {
 	req := apireq.ListContact{}
@@ -36,4 +65,60 @@ func ListContact(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, res)
+}
+
+func GetContact(c *gin.Context) {
+	contactIdStr := c.Param("id")
+	contactId, err := strconv.Atoi(contactIdStr)
+	if err != nil {
+		paramErr := er.NewAppErr(400, er.ErrorParamInvalid, "contact id format error.", err)
+		_ = c.Error(paramErr)
+		return
+	}
+
+	cr := contactRepo.NewRepository()
+	cs := contactSrv.NewService(cr)
+	res, err := cs.Get(contactId)
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
+}
+
+func EditContact(c *gin.Context) {
+	contactIdStr := c.Param("id")
+	contactId, err := strconv.Atoi(contactIdStr)
+	if err != nil {
+		paramErr := er.NewAppErr(400, er.ErrorParamInvalid, "contact id format error.", err)
+		_ = c.Error(paramErr)
+		return
+	}
+
+	req := apireq.EditContact{}
+	err = c.BindJSON(&req)
+	if err != nil {
+		err = er.NewAppErr(400, er.ErrorParamInvalid, err.Error(), err)
+		_ = c.Error(err)
+		return
+	}
+
+	// 參數驗證
+	err = valider.Validate.Struct(req)
+	if err != nil {
+		err = er.NewAppErr(400, er.ErrorParamInvalid, err.Error(), err)
+		_ = c.Error(err)
+		return
+	}
+
+	cr := contactRepo.NewRepository()
+	cs := contactSrv.NewService(cr)
+	err = cs.Edit(contactId, &req)
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{})
 }
--- route/contact.go
+++ route/contact.go
@@ -9,7 +9,19 @@ func ContactV1(r *gin.Engine) {
 	v1Auth := r.Group("/v1/contacts")
 	//v1Auth.Use(middleware.TokenAuth())
 
+	v1Auth.POST("/", func(c *gin.Context) {
+		apiV1.AddContact(c)
+	})
+
 	v1Auth.GET("/", func(c *gin.Context) {
 		apiV1.ListContact(c)
+	})
+
+	v1Auth.GET("/:id", func(c *gin.Context) {
+		apiV1.GetContact(c)
+	})
+
+	v1Auth.PUT("/:id", func(c *gin.Context) {
+		apiV1.EditContact(c)
 	})
 }
